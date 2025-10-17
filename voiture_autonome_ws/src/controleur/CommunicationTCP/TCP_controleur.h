71ebd34c0f8a1ad0cb559571997607b3315bff43
LaChaise <thibault.pelenc@gmail.com>
2025-10-17 11:50:16 +0200

    Implementation résolution pbm thread

--- voiture_autonome_ws/src/controleur/CommunicationTCP/TCP_controleur.h
+++ voiture_autonome_ws/src/controleur/CommunicationTCP/TCP_controleur.h
@@ -4,4 +4,4 @@ void* receive_thread(void* arg);
 // Boucle interactive pour envoyer consignes/itinéraires
 void* boucle_interactive(void* arg);
 
-int main_communication_controleur();
+void* main_communication_controleur(void* arg);
